ea614777ec870ecffef69d666531f3fb7c162570
Shubham <pps.shubham01@gmail.com>
2024-02-10 13:10:04 +0530

    ResponseData

--- lib/services/chat_service.dart
+++ lib/services/chat_service.dart
@@ -1,75 +1,477 @@
-
-
-
-
-
-
-// http://192.168.1.142:8004/tf/core/api/service/dev/v1/chats/v1/collaborators/get/?
-// Get Method
-// Content-Type:application/json
-// Token 1938157ea06eae6a70567e31a10c2fbb287bca5b
 //
 //
 //
 //
 //
+//
+// // http://192.168.1.142:8004/tf/core/api/service/dev/v1/chats/v1/collaborators/get/?
+// // Get Method
+// // Content-Type:application/json
+// // Token 1938157ea06eae6a70567e31a10c2fbb287bca5b
+// //
+// //
+// //
+// //
+// //
+// //
+// // {
+// // "status": 200,
+// // "message": "Obtained users and groups.",
+// // "data": {
+// // "groups": [
+// // {
+// // "group_name": "vegetables",
+// // "group_category_name": "USER_PRIVATE",
+// // "group_desc": "User Group",
+// // "group_remark": "None",
+// // "id": 634,
+// // "username": "My_brownsugar",
+// // "first_name": "Ice",
+// // "last_name": "creame",
+// // "last_message_activity": "2023-12-05T10:08:43.992743Z",
+// // "members": [
+// // 551,
+// // 553
+// // ],
+// // "unread": 0
+// // }
+// // ],
+// // "individuals": [
+// // {
+// // "first_name": "Ringan",
+// // "last_message_activity": "2024-02-01T06:45:46.971241Z",
+// // "last_name": "Bateta",
+// // "username": "xenodochialkilby",
+// // "_id": 553,
+// // "unread": 0
+// // },
+// // {
+// // "first_name": "jahangir",
+// // "last_message_activity": "2024-02-01T06:44:07.225635Z",
+// // "last_name": "gavar",
+// // "username": "unruffleddewdney",
+// // "_id": 610,
+// // "unread": 0
+// // },
+// // {
+// // "first_name": "Shubham",
+// // "last_message_activity": "2023-11-04T04:41:36.309976Z",
+// // "last_name": "Prajapati",
+// // "username": "lovingswanson",
+// // "_id": 551,
+// // "unread": 0
+// // },
+// // {
+// // "first_name": "krups",
+// // "last_message_activity": "2023-11-04T04:39:45.404317Z",
+// // "last_name": "hinsu",
+// // "username": "crankyeasley",
+// // "_id": 473,
+// // "unread": 0
+// // }
+// // ],
+// // "self_id": 552
+// // }
+// // }
+//
+//
+//
+// // ChatMessages
 //
 // {
-// "status": 200,
-// "message": "Obtained users and groups.",
-// "data": {
-// "groups": [
+// "message": "Obtained messages",
+// "data": [
 // {
-// "group_name": "vegetables",
-// "group_category_name": "USER_PRIVATE",
-// "group_desc": "User Group",
-// "group_remark": "None",
-// "id": 634,
-// "username": "My_brownsugar",
-// "first_name": "Ice",
-// "last_name": "creame",
-// "last_message_activity": "2023-12-05T10:08:43.992743Z",
-// "members": [
-// 551,
-// 553
-// ],
-// "unread": 0
-// }
-// ],
-// "individuals": [
-// {
-// "first_name": "Ringan",
-// "last_message_activity": "2024-02-01T06:45:46.971241Z",
-// "last_name": "Bateta",
-// "username": "xenodochialkilby",
-// "_id": 553,
-// "unread": 0
+// "id": 1015263,
+// "is_read": null,
+// "message_from_id": 553,
+// "message_to_id": 552,
+// "in_group": false,
+// "_id": 1015263,
+// "name": "Ringan Bateta",
+// "starred": false,
+// "mentions": [],
+// "message_body": "hi",
+// "create_date": "2023-11-04T04:47:08.011645Z",
+// "is_forwarded": false,
+// "edited": false,
+// "parent_msg_detail": null,
+// "is_meeting_missed_call": true,
+// "is_meeting_rec": false,
+// "outgoing_call": false,
+// "incoming_call": false,
+// "meeting_start_time": null,
+// "meeting_end_time": null,
+// "attachements": [],
+// "is_reply_to": false,
+// "reply_to_id": null,
+// "is_private_reply": false,
+// "reply_source_group": null,
+// "reply_attachement_id": null
 // },
 // {
-// "first_name": "jahangir",
-// "last_message_activity": "2024-02-01T06:44:07.225635Z",
-// "last_name": "gavar",
-// "username": "unruffleddewdney",
-// "_id": 610,
-// "unread": 0
+// "id": 1015264,
+// "is_read": null,
+// "message_from_id": 553,
+// "message_to_id": 552,
+// "in_group": false,
+// "_id": 1015264,
+// "name": "Ringan Bateta",
+// "starred": false,
+// "mentions": [],
+// "message_body": "Hi Icecreame",
+// "create_date": "2023-11-04T04:47:33.802874Z",
+// "is_forwarded": false,
+// "edited": false,
+// "parent_msg_detail": null,
+// "is_meeting_missed_call": true,
+// "is_meeting_rec": false,
+// "outgoing_call": false,
+// "incoming_call": false,
+// "meeting_start_time": null,
+// "meeting_end_time": null,
+// "attachements": [],
+// "is_reply_to": false,
+// "reply_to_id": null,
+// "is_private_reply": false,
+// "reply_source_group": null,
+// "reply_attachement_id": null
 // },
 // {
-// "first_name": "Shubham",
-// "last_message_activity": "2023-11-04T04:41:36.309976Z",
-// "last_name": "Prajapati",
+// "id": 1015266,
+// "is_read": true,
+// "message_from_id": 552,
+// "message_to_id": 553,
+// "in_group": false,
+// "_id": 1015266,
+// "name": "Ice creame",
+// "starred": false,
+// "mentions": [],
+// "message_body": "hey",
+// "create_date": "2023-11-04T04:55:17.366820Z",
+// "is_forwarded": false,
+// "edited": false,
+// "parent_msg_detail": null,
+// "is_meeting_missed_call": true,
+// "is_meeting_rec": false,
+// "outgoing_call": false,
+// "incoming_call": false,
+// "meeting_start_time": null,
+// "meeting_end_time": null,
+// "attachements": [],
+// "is_reply_to": false,
+// "reply_to_id": null,
+// "is_private_reply": false,
+// "reply_source_group": null,
+// "reply_attachement_id": null
+// },
+// {
+// "id": 1015267,
+// "is_read": true,
+// "message_from_id": 552,
+// "message_to_id": 553,
+// "in_group": false,
+// "_id": 1015267,
+// "name": "Ice creame",
+// "starred": false,
+// "mentions": [],
+// "message_body": "ohh hi Ringan",
+// "create_date": "2023-11-04T04:55:36.010179Z",
+// "is_forwarded": false,
+// "edited": false,
+// "parent_msg_detail": null,
+// "is_meeting_missed_call": true,
+// "is_meeting_rec": false,
+// "outgoing_call": false,
+// "incoming_call": false,
+// "meeting_start_time": null,
+// "meeting_end_time": null,
+// "attachements": [],
+// "is_reply_to": false,
+// "reply_to_id": null,
+// "is_private_reply": false,
+// "reply_source_group": null,
+// "reply_attachement_id": null
+// },
+// {
+// "id": 1015268,
+// "is_read": true,
+// "message_from_id": 552,
+// "message_to_id": 553,
+// "in_group": false,
+// "_id": 1015268,
+// "name": "Ice creame",
+// "starred": false,
+// "mentions": [
+// {
 // "username": "lovingswanson",
-// "_id": 551,
-// "unread": 0
-// },
-// {
-// "first_name": "krups",
-// "last_message_activity": "2023-11-04T04:39:45.404317Z",
-// "last_name": "hinsu",
-// "username": "crankyeasley",
-// "_id": 473,
-// "unread": 0
+// "start": 0,
+// "end": 14,
+// "name": "Shubham Prajapati",
+// "id": 551
 // }
 // ],
-// "self_id": 552
+// "message_body": "@lovingswanson",
+// "create_date": "2023-11-04T05:00:06.321638Z",
+// "is_forwarded": false,
+// "edited": false,
+// "parent_msg_detail": null,
+// "is_meeting_missed_call": true,
+// "is_meeting_rec": false,
+// "outgoing_call": false,
+// "incoming_call": false,
+// "meeting_start_time": null,
+// "meeting_end_time": null,
+// "attachements": [],
+// "is_reply_to": false,
+// "reply_to_id": null,
+// "is_private_reply": false,
+// "reply_source_group": null,
+// "reply_attachement_id": null
+// },
+// {
+// "id": 1015891,
+// "is_read": null,
+// "message_from_id": 553,
+// "message_to_id": 552,
+// "in_group": false,
+// "_id": 1015891,
+// "name": "Ringan Bateta",
+// "starred": false,
+// "mentions": [],
+// "message_body": "hi",
+// "create_date": "2023-11-23T08:16:51.807642Z",
+// "is_forwarded": false,
+// "edited": false,
+// "parent_msg_detail": null,
+// "is_meeting_missed_call": true,
+// "is_meeting_rec": false,
+// "outgoing_call": false,
+// "incoming_call": false,
+// "meeting_start_time": null,
+// "meeting_end_time": null,
+// "attachements": [],
+// "is_reply_to": false,
+// "reply_to_id": null,
+// "is_private_reply": false,
+// "reply_source_group": null,
+// "reply_attachement_id": null
+// },
+// {
+// "id": 1015892,
+// "is_read": null,
+// "message_from_id": 553,
+// "message_to_id": 552,
+// "in_group": false,
+// "_id": 1015892,
+// "name": "Ringan Bateta",
+// "starred": false,
+// "mentions": [],
+// "message_body": "hiiodsgojsdfnocllx",
+// "create_date": "2023-11-23T08:18:14.221727Z",
+// "is_forwarded": false,
+// "edited": false,
+// "parent_msg_detail": null,
+// "is_meeting_missed_call": true,
+// "is_meeting_rec": false,
+// "outgoing_call": false,
+// "incoming_call": false,
+// "meeting_start_time": null,
+// "meeting_end_time": null,
+// "attachements": [],
+// "is_reply_to": false,
+// "reply_to_id": null,
+// "is_private_reply": false,
+// "reply_source_group": null,
+// "reply_attachement_id": null
+// },
+// {
+// "id": 1016148,
+// "is_read": true,
+// "message_from_id": 552,
+// "message_to_id": 553,
+// "in_group": false,
+// "_id": 1016148,
+// "name": "Ice creame",
+// "starred": false,
+// "mentions": [],
+// "message_body": "hi",
+// "create_date": "2023-12-05T09:12:28.977754Z",
+// "is_forwarded": false,
+// "edited": false,
+// "parent_msg_detail": null,
+// "is_meeting_missed_call": true,
+// "is_meeting_rec": false,
+// "outgoing_call": false,
+// "incoming_call": false,
+// "meeting_start_time": null,
+// "meeting_end_time": null,
+// "attachements": [],
+// "is_reply_to": false,
+// "reply_to_id": null,
+// "is_private_reply": false,
+// "reply_source_group": null,
+// "reply_attachement_id": null
+// },
+// {
+// "id": 1016150,
+// "is_read": null,
+// "message_from_id": 553,
+// "message_to_id": 552,
+// "in_group": false,
+// "_id": 1016150,
+// "name": "Ringan Bateta",
+// "starred": false,
+// "mentions": [],
+// "message_body": "Hi Icecreame",
+// "create_date": "2023-12-05T10:05:22.958235Z",
+// "is_forwarded": false,
+// "edited": false,
+// "parent_msg_detail": null,
+// "is_meeting_missed_call": true,
+// "is_meeting_rec": false,
+// "outgoing_call": false,
+// "incoming_call": false,
+// "meeting_start_time": null,
+// "meeting_end_time": null,
+// "attachements": [],
+// "is_reply_to": false,
+// "reply_to_id": null,
+// "is_private_reply": false,
+// "reply_source_group": null,
+// "reply_attachement_id": null
+// },
+// {
+// "id": 1020141,
+// "is_read": false,
+// "message_from_id": 552,
+// "message_to_id": 553,
+// "in_group": false,
+// "_id": 1020141,
+// "name": "Ice creame",
+// "starred": false,
+// "mentions": [],
+// "message_body": "-",
+// "create_date": "2024-02-01T06:43:35.793936Z",
+// "is_forwarded": false,
+// "edited": false,
+// "parent_msg_detail": null,
+// "is_meeting_missed_call": true,
+// "is_meeting_rec": false,
+// "outgoing_call": false,
+// "incoming_call": false,
+// "meeting_start_time": null,
+// "meeting_end_time": null,
+// "attachements": [
+// {
+// "attachement_id": 9091,
+// "content_id": 37309,
+// "content_name": "images",
+// "content_type": 1,
+// "rel_ref": "c6527554-d0fc-4adf-a894-9d287a4c530d",
+// "is_forwarded": false,
+// "versions": 0
 // }
+// ],
+// "is_reply_to": false,
+// "reply_to_id": null,
+// "is_private_reply": false,
+// "reply_source_group": null,
+// "reply_attachement_id": null
+// },
+// {
+// "id": 1020143,
+// "is_read": false,
+// "message_from_id": 552,
+// "message_to_id": 553,
+// "in_group": false,
+// "_id": 1020143,
+// "name": "Ice creame",
+// "starred": false,
+// "mentions": [],
+// "message_body": "-",
+// "create_date": "2024-02-01T06:45:46.971241Z",
+// "is_forwarded": false,
+// "edited": false,
+// "parent_msg_detail": null,
+// "is_meeting_missed_call": true,
+// "is_meeting_rec": false,
+// "outgoing_call": false,
+// "incoming_call": false,
+// "meeting_start_time": null,
+// "meeting_end_time": null,
+// "attachements": [
+// {
+// "attachement_id": 9093,
+// "content_id": 195455,
+// "content_name": "shoew2.jpg",
+// "content_type": 2,
+// "rel_ref": "89e22674-8a56-4a33-a90c-f4789a191e4c",
+// "is_forwarded": false,
+// "versions": 0
 // }
+// ],
+// "is_reply_to": false,
+// "reply_to_id": null,
+// "is_private_reply": false,
+// "reply_source_group": null,
+// "reply_attachement_id": null
+// },
+// {
+// "id": 1020479,
+// "is_read": false,
+// "message_from_id": 552,
+// "message_to_id": 553,
+// "in_group": false,
+// "_id": 1020479,
+// "name": "Ice creame",
+// "starred": false,
+// "mentions": [],
+// "message_body": "hi",
+// "create_date": "2024-02-10T05:11:37.955893Z",
+// "is_forwarded": false,
+// "edited": false,
+// "parent_msg_detail": null,
+// "is_meeting_missed_call": true,
+// "is_meeting_rec": false,
+// "outgoing_call": false,
+// "incoming_call": false,
+// "meeting_start_time": null,
+// "meeting_end_time": null,
+// "attachements": [],
+// "is_reply_to": false,
+// "reply_to_id": null,
+// "is_private_reply": false,
+// "reply_source_group": null,
+// "reply_attachement_id": null
+// }
+// ],
+// "other": {
+// "request_detail": {
+// "message_to_id": 553,
+// "in_group": false,
+// "range_from": 0,
+// "range_to": 20,
+// "tf_name": "xenodochialkilby",
+// "org_id": null,
+// "message_id": null,
+// "message_date": null,
+// "starred": false
+// },
+// "total_messages": 12,
+// "unread": [],
+// "trusted": true,
+// "blocked": false,
+// "hide_trusted": false,
+// "page_number": null
+// },
+// "status": 200
+// }
+//
+//
+//
+// http://192.168.1.142:8004/tf/core/api/service/dev/v1/chats/v1/messages/get/
+// post
+//
+// payLoad
+// {"message_to_id":553,"in_group":false,"range_from":0,"range_to":20,"tf_name":"xenodochialkilby","org_id":null,"message_id":null}
